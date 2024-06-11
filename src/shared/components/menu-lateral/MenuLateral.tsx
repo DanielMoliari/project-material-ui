b3cf2deec435002c81d517eb29fa515fbce38ac2
Daniel Barbosa <daniel.barbosa@a4pm.com.br>
2024-06-11 11:06:07 -0300

    criando menu lateral

---
+++ src/shared/components/menu-lateral/MenuLateral.tsx
@@ -0,0 +1,63 @@
+import {
+  Avatar,
+  Box,
+  Divider,
+  Drawer,
+  Icon,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  useTheme,
+} from "@mui/material";
+
+interface IMenuLateralProps {
+  children: React.ReactNode;
+}
+
+export const MenuLateral: React.FC<IMenuLateralProps> = ({ children }) => {
+  const theme = useTheme();
+
+  return (
+    <>
+      <Drawer variant="permanent">
+        <Box
+          width={theme.spacing(28)}
+          height="100%"
+          display="flex"
+          flexDirection="column"
+        >
+          <Box
+            width="100%"
+            height={theme.spacing(20)}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <Avatar
+              sx={{ height: theme.spacing(12), width: theme.spacing(12) }}
+              src="https://gitlab.devrgesus.com.br/uploads/-/system/user/avatar/294/avatar.png?width=96"
+            />
+          </Box>
+
+          <Divider />
+
+          <Box flex={1}>
+            <List component="nav">
+              <ListItemButton>
+                <ListItemIcon>
+                  <Icon>home</Icon>
+                </ListItemIcon>
+                <ListItemText primary="Página inicial" />
+              </ListItemButton>
+            </List>
+          </Box>
+        </Box>
+      </Drawer>
+
+      <Box height="100vh" marginLeft={theme.spacing(28)}>
+        {children}
+      </Box>
+    </>
+  );
+};
