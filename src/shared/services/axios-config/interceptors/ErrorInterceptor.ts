4319327fd757522c30467149fe8f7020af9bbfa4
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-06-19 18:23:35 -0300

    feat: configurações bases do axios e instalação dele

---
+++ src/shared/services/axios-config/interceptors/ErrorInterceptor.ts
@@ -0,0 +1,14 @@
+import { AxiosError } from "axios";
+
+export const errorInterceptor = (error: AxiosError) => {
+    
+    if(error.message === "Network Error"){
+        return Promise.reject(new Error("Erro de conexão."));
+    }
+
+    if(error.response?.status === 401){
+        return Promise.reject(new Error("Autenticação negada."));
+    }
+
+    return Promise.reject(error);
+}
