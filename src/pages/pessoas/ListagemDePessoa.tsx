98a336295acc0494773ba0e99f81d5d923ea47cd
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-06-20 23:21:24 -0300

    feat: useDebounce criado

--- src/pages/pessoas/ListagemDePessoa.tsx
+++ src/pages/pessoas/ListagemDePessoa.tsx
@@ -3,23 +3,26 @@ import { FerramentasDaListagem } from "../../shared/components"
 import { LayoutBaseDePagina } from "../../shared/layouts"
 import { useEffect, useMemo } from "react";
 import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
+import { useDebounce } from "../../shared/hooks";
 
 export const ListagemDePessoa: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
+    const { debounce } = useDebounce(3000, true);
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     },[searchParams])
 
     useEffect(() => {
-
-        PessoasService.getAll(1, busca)
-        .then((result) => {
-            if (result instanceof Error) {
-                alert(result.message);
-                return;
-            }
-            console.log(result);
+        debounce(() => {
+            PessoasService.getAll(1, busca)
+            .then((result) => {
+                if (result instanceof Error) {
+                    alert(result.message);
+                    return;
+                }
+                console.log(result);
+            })
         })
     }, [busca])
 
