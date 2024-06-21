06e7195e28a27f4e25a1398348c22187b70550c3
Daniel PC <danielmoliaribarbosa@gmail.com>
2024-06-20 23:04:33 -0300

    feat: alterando os nomes de cidade para pessoas

--- src/pages/pessoas/ListagemDePessoa.tsx
+++ src/pages/pessoas/ListagemDePessoa.tsx
@@ -1,14 +1,27 @@
 import { useSearchParams } from "react-router-dom"
 import { FerramentasDaListagem } from "../../shared/components"
 import { LayoutBaseDePagina } from "../../shared/layouts"
-import { useMemo } from "react";
+import { useEffect, useMemo } from "react";
+import { PessoasService } from "../../shared/services/api/pessoas/PessoasService";
 
-export const ListagemDeCidade: React.FC = () => {
+export const ListagemDePessoa: React.FC = () => {
     const [searchParams, setSearchParams] = useSearchParams();
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     },[searchParams])
+
+    useEffect(() => {
+
+        PessoasService.getAll()
+        .then((result) => {
+            if (result instanceof Error) {
+                alert(result.message);
+                return;
+            }
+            console.log(result);
+        })
+    }, [])
 
     return (
         <LayoutBaseDePagina titulo="Listagem de cidades" barraDeFerramentas={
